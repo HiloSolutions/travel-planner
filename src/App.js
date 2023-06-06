f2271ecb419d43c4495470e80f22361f4aac42dd
Lauren Johnston <hilotraining@outlook.com>
2023-06-06 10:22:18 -0600

    set up authentication with auth0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Route } from "react-router-dom";
+import Nav from './components/Nav';
+import LoginButton from './components/buttons/LoginButton';
+import LogoutButton from './components/buttons/LogoutButton';
+import Profile from './views/Profile';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
     </div>
   );
 }
