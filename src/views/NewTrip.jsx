08dd6ad8c94a96610ce6b6fe2a596715c30cdbf5
Lauren Johnston <hilotraining@outlook.com>
2023-06-08 14:23:44 -0600

    connected to express backend

--- src/views/NewTrip.jsx
+++ src/views/NewTrip.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import Loading from '../components/Loading';
 import Map from '../components/map-functionality/Map';
 import SearchBar from '../components/map-functionality/SearchBar';
+import { getPlacesAPI } from '../api/getPlacesApi';
 
 
 const NewTrip = () => {
   const { isAuthenticated, isLoading } = useAuth0();
   const [searchQuery, setSearchQuery] = useState("");
-
+  const [places, setPlaces] = useState(null);
+  
+  console.log(places);
+  useEffect(() => {
+    const result = getPlacesAPI();
+    setPlaces(result);
+  }, []);
 
   if (isLoading) {
     return <Loading />;
